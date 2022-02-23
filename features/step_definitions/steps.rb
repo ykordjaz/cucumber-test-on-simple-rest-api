47c453417ec640375afafbf63f36cabae25bea58
Yas <ykordjaz@gmail.com>
2022-02-24 00:46:27 +0100

    added a whole bunch of comments to remember in the future

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,51 +1,41 @@
 require "net/http"
 require "uri"
 
-# Given('the service is running') do
-#     uri = URI.parse('http://127.0.0.1:3000/api/v1/healthcheck')
-    
-#     http = Net::HTTP.new(uri.host, uri.port)
-#     get_healthcheck_request = Net::HTTP::Get.new(uri.request_uri)
-#     # Get.new creates a new request object
-#     response = http.request(get_healthcheck_request)
-#     expect(response.code).to eq "200"
-#   end
-  
-#   When('I send GET HTTP request') do
-#     uri = URI.parse('http://127.0.0.1:3000/api/v1/users')
-    
-#     http = Net::HTTP.new(uri.host, uri.port)
-#     get_users = Net::HTTP::Get.new(uri.request_uri)
-#     # Get.new creates a new request object and store it in an instance var
-    
-#     response = http.request(get_users)
-#     @response = response
-#     # expect(response.code).to eq "200"
-
-#   end
-  
-#   Then('I receive valid HTTP response code {int}') do |expected_http_code|
-#     expect(@response.code).to eq(expected_http_code.to_s)
-#   end
-  
-#   Then('I get a list of all users') do
-#     expect(@response.body).to eq "[]"
-#   end
-  
-
-
-#  this is for the POST method
 
 Given('the service is running') do
-    uri = URI.parse('http://127.0.0.1:3000/api/v1/healthcheck')
-    
-    http = Net::HTTP.new(uri.host, uri.port)
-    get_healthcheck_request = Net::HTTP::Get.new(uri.request_uri)
-    # Get.new creates a new request object
-    response = http.request(get_healthcheck_request)
-    expect(response.code).to eq "200"
+  uri = URI.parse('http://127.0.0.1:3000/api/v1/healthcheck')
+  
+  http = Net::HTTP.new(uri.host, uri.port)
+  get_healthcheck_request = Net::HTTP::Get.new(uri.request_uri)
+  # Get.new creates a new request object
+  response = http.request(get_healthcheck_request)
+  expect(response.code).to eq "200"
 end
   
+When('I send GET HTTP request') do
+    uri = URI.parse('http://127.0.0.1:3000/api/v1/users')
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    get_users = Net::HTTP::Get.new(uri.request_uri)
+    # Get.new creates a new request object and store it in an instance var
+    
+    response = http.request(get_users)
+    @response = response
+    # expect(response.code).to eq "200"
+
+end
+  
+Then('I receive valid HTTP response code {int}') do |expected_response_code|
+    # response.code is the 200, 400, etc code and it's a string.
+    expect(@response.code).to eq(expected_response_code.to_s)
+end
+  
+Then('I get a list of all users') do
+    expect(@response.body).to eq "[]"
+end
+  
+
+# #  this is for the POST method
   
 When('I send POST HTTP request') do
     
@@ -70,11 +60,6 @@ When('I send POST HTTP request') do
     response = http.request(request_with_user_data)
     # storing response as instance variable to be accessed later
     @response = response
-end
-  
-Then('I receive valid HTTP response code {int}') do |expected_response_code|
-    # response.code is the 200, 400, etc code and it's a string.
-    expect(@response.code).to eq(expected_response_code.to_s)
 end
   
 Then('I get info on that specific user') do
--- features/user.feature
+++ features/user.feature
@@ -1,10 +1,10 @@
 Feature: Test CRUD methods in "Simple REST API" (the name of my API - consisting of user, book, and subject models)
 
-# Scenario: Fetch all users
-#     Given the service is running
-#     When I send GET HTTP request
-#     Then I receive valid HTTP response code 200  
-#     And I get a list of all users
+Scenario: Fetch all users
+    Given the service is running
+    When I send GET HTTP request
+    Then I receive valid HTTP response code 200  
+    And I get a list of all users
 
 Scenario: Create a new user
     Given the service is running
@@ -22,4 +22,4 @@ Scenario: Create a new user
 #     Given the service is running
 #     When I send DELETE HTTP request
 #     And I send GET HTTP request
-#     Then I receive valid HTTP response code 404  
+#     Then I receive valid HTTP response code 404     
